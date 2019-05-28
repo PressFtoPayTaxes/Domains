62edb150e8a93def4494bfe99e016f1ec1b6582a
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-05-28 18:04:21 +0600

    Init

---
+++ LargeCalculationsApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace LargeCalculationsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100000;
+
+            BigInteger numbersFactorial = 1;
+
+            for (int i = 1; i < number; i++)
+            {
+                numbersFactorial *= i;
+            }
+        }
+    }
+}
