9d8b37c4438121cdf7e5cfd68152a594049143a4
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-05-28 18:03:35 +0600

    Init

---
+++ FileDownloadApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDownloadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://file-examples.com/wp-content/uploads/2017/02/zip_2MB.zip", "zip_2MB.zip");
+            }
+        }
+    }
+}
